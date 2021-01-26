55186a89fde8676e2c85f13f6d62340f1b6b3421
Brad Robinson <contact@toptensoftware.com>
2021-01-26 14:36:51 +1100

    Added ability to change main() called by sysinit

--- lib/sysinit.cpp
+++ lib/sysinit.cpp
@@ -221,8 +221,12 @@ void sysinit (void)
 		(**pFunc) ();
 	}
 
-	extern int main (void);
-	if (main () == EXIT_REBOOT)
+	#ifndef MAINPROC
+	#define MAINPROC main
+	#endif
+	extern int MAINPROC (void);
+	
+	if (MAINPROC () == EXIT_REBOOT)
 	{
 		if (IsChainBootEnabled ())
 		{
